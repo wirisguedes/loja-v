941cb374661822de921b4181e8a97d86121e0fdc
irisg <irisg@Iris>
2024-11-03 13:56:36 -0300

    implementacao Pessoa

---
+++ src/main/java/com/loja_v/model/Acesso.java
@@ -0,0 +1,70 @@
+package com.loja_v.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+	
+	@Column(name = "decricao", nullable = false)
+	private String decricao; //Acesso ex ROLE_ADMIN
+
+	@Override
+	public String getAuthority() {
+		
+		return this.decricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDecricao() {
+		return decricao;
+	}
+
+	public void setDecricao(String decricao) {
+		this.decricao = decricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Acesso other = (Acesso) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
