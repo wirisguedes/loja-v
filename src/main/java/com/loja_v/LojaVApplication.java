9629f4d876d31b87b19a9eceefce9a665bca4904
irisg <irisg@Iris>
2024-11-03 09:52:58 -0300

    estrutura inicial

---
+++ src/main/java/com/loja_v/LojaVApplication.java
@@ -0,0 +1,13 @@
+package com.loja_v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loja_v/LojaVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja_v;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
