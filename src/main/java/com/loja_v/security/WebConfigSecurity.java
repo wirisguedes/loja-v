1d25c88426399564c0b7f00c68a86cdb488ac0b1
irisg <irisg@Iris>
2024-11-15 11:51:29 -0300

    implementacao WebConfigSecurity

--- src/main/java/com/loja_v/security/WebConfigSecurity.java
+++ src/main/java/com/loja_v/security/WebConfigSecurity.java
@@ -2,17 +2,32 @@ package com.loja_v.security;
 
 import javax.servlet.http.HttpSessionListener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.loja_v.service.ImplementacaoUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener{
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService; 
+	
+	/*Consultar user no banco com spring security*/
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
