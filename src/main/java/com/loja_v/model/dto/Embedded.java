ac3ab87b937dbef83767e18bfae1f171fa3dee2a
Iris <wirisguedes@gmail.com>
2024-12-25 11:56:39 -0300

    implementacao BoletoGeradoApiJuno

---
+++ src/main/java/com/loja_v/model/dto/Embedded.java
@@ -0,0 +1,22 @@
+package com.loja_v.model.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Embedded implements Serializable{
+	
+private static final long serialVersionUID = 1L;
+	
+	private List<ConteudoBoletoJuno> charges = new ArrayList<ConteudoBoletoJuno>();
+	
+	public void setCharges(List<ConteudoBoletoJuno> charges) {
+		this.charges = charges;
+	}
+	
+	public List<ConteudoBoletoJuno> getCharges() {
+		return charges;
+	}
+
+
+}
